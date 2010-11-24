7b0c299f5e64ae28f195514d70c6f827affec825
Andrew Ross <fosslc@gmail.com>
2010-11-24 14:55:03 -0500

    ------------------------------------------------------------------------ r3760 | loera01 | 2010-10-29 12:30:49 -0400 (Fri, 29 Oct 2010) | 9 lines
    Cross-integration of change 124658 from ingres10.
    
    A section of code in SQLSpecialColumns_InternalCall() jumped to the exit
    point only upon an error, when the code should have exited regardless of
    status.
    
    I changed the code to exit regardless of status.
    
    ------------------------------------------------------------------------

--- src/common/odbc/driver/catalog.c
+++ src/common/odbc/driver/catalog.c
@@ -271,6 +271,10 @@
 **     24-Aug-2010 (Ralph Loen) Bug 124300
 **         Add SQL_MAX_PROCEDURE_NAME_LEN to select query in 
 **         CatGetDBCapabilities().
+**     27-Oct-2010 (Ralph Loen) Bug 124658
+**         In SQLSpecialColumns_InternalCall(), return after calling
+**         CatExecDirect() in the case of SQL_ROWVER or SQL_ROWID for
+**         transaction scope.
 */
 
 /*
@@ -2143,8 +2147,8 @@ RETCODE SQL_API SQLSpecialColumns_InternalCall(
             CatRemove (szSqlStr, AND);
 
         retcode = CatExecDirect (pstmt, szSqlStr, CATLG_SPECCOL);
-        if (retcode != SQL_SUCCESS)
-            goto routine_exit;
+
+        goto routine_exit;
     }
 
     /*
